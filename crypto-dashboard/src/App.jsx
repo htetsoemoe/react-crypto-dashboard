a2d801c9e5af80b5a7e3b58ff58e3be26326f322
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-06-20 15:47:17 +0630

    project setup with tailwindcss

---
+++ crypto-dashboard/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h2 className='text-2xl font-semibold text-center'>Crypto Dashboard</h2>
+    </div>
+  )
+}
+
+export default App
